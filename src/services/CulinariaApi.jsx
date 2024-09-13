8b9dfbf5f97ca269234008aa11b6f8a4b6dd86e7
rafinha24 <rafaelazanettibau@gmail.com>
2024-09-13 08:45:13 -0300

    ee

--- src/services/CulinariaApi.jsx
+++ src/services/CulinariaApi.jsx
@@ -1,7 +1,9 @@
 export const getCulinariaData = async (nome) => {
     try{
         //mude esse fetch para o link que o jorge pedir
-        const response = await fetch(`https://8d19-201-22-100-109.ngrok-free.app/api/receita/${nome}`);
+        const response = await fetch(`https://8d19-201-22-100-109.ngrok-free.app/api/receita/${nome}`, {headers: {
+            "ngrok-skip-browser-warning" : undefined
+        }});
         const data = await response.json();
         return data;
     } catch(error){
